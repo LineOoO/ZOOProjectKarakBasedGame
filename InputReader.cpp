0bfad35df6e9ad6cdcd53e3ced7b39d119e4dfc7
Elijaah <116018100+darksoothingshadow@users.noreply.github.com>
2025-12-16 18:50:25 +0000

    feat: add comments to InputReader & Renderer

--- InputReader.cpp
+++ InputReader.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 InputCommand InputReader::readInput() {
+    // ask for user input in the console
     char input;
     std::cin >> input;
     
+    // process the char input and return as an enum value
     switch (input) {
         case 'u': return InputCommand::UP;
         case 'd': return InputCommand::DOWN;
--- Renderer.cpp
+++ Renderer.cpp
@@ -3,24 +3,31 @@
 #include "Card.h"
 #include "Renderer.h"
 
+// game introduction message :)
 std::string Renderer::renderIntroduction() {
     return "Welcome to ZOO\nYour task as a hero is to navigate a maze, collect items and kill as many enemies as possible.\nBe quick!\n\n";
 }
 
+// render each room after per Hero move
 void Renderer::renderRoom(Card* card, bool hasEnemy, bool hasItem) {
     std::array<bool, 4> exits = card->getExits();
     int width = 9;
     int height = 9;
 
+    // 9x9 room
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             if (x == 4 && y == 4) {
+                // render hero position
                 std::cout << "H ";
             } else if (hasEnemy && x == 6 && y == 4) {
+                // render enemy position
                 std::cout << "E ";
             } else if (hasItem && x == 4 && y == 2) {
+                // render item position
                 std::cout << "I ";
             } else {
+                // render room border (exclude wall for each exit)
                 bool isWall = (y == 0 || y == 8 || x == 0 || x == 8);
                 bool isExit = false;
 
@@ -40,6 +47,7 @@ void Renderer::renderRoom(Card* card, bool hasEnemy, bool hasItem) {
     }
 }
 
+// render game over message
 std::string Renderer::renderEnd(bool hasWon) {
     if (hasWon) {
         return "You won, congrats! Collect your reward (6 ECTS have just arrived to your student account)!\n";
